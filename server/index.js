c196e288e71847c523f353617ef561ca684a2813
Student <jsitbon@qcc.cuny.edu>
2019-08-01 13:41:03 -0400

    added puppeteer and got it to work. One weird thing is that you need to use http instead of https for redirect to happen ex)abc.com -> abc.go.com

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const getUser = require('../models').getUser
 const addUrl = require('../models').addUrl
 const deleteUrl = require('../models').deleteUrl
 const updateCheck = require('../models').updateCheck
-const axios = require('axios')
-const cheerio = require('cheerio')
+const puppeteer = require('puppeteer')
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -35,17 +34,68 @@ app.delete('/api', (req, res) => {
   res.send('deleted!')
 })
 
-app.post('/title', (req, res) => {
+app.post('/title', async (req, res) => {
   let url = req.body.url;
-  axios.get(url).then(d => {
-    // console.log(findTitle(d.data))
-    res.send(findTitle(d.data))
-  }).catch(e => res.send(e))
+  console.log('url', url)
+  let title = await getTitlePup(url)
+  res.send(title)
 })
 
-function findTitle(str) {
-  const $ = cheerio.load(str)
-  return $.html()
+
+// function findTitle(str) {
+//   const $ = cheerio.load(str)
+//   return $.html()
+// }
+
+async function getTitlePup(url) {
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  console.log('url in puppeteer', url)
+  let mainUrlStatus;
+  await page.setRequestInterception(true);
+  page.on("request", request => {
+    const url = request.url();
+    console.log("request url:", url);
+    request.continue();
+  });
+  page.on("requestfailed", request => {
+    const url = request.url();
+    console.log("request failed url:", url);
+  });
+  page.on("response", response => {
+    const request = response.request();
+    const url = request.url();
+    const status = response.status();
+    console.log("response url:", url, "status:", status);
+		if (url === mainUrl) {
+			mainUrlStatus = status;
+		}
+  });
+  // const title = await page.evaluate(() => {
+  //   return document.getElementsByTagName('title')[0].text
+  // })
+  
+  await page.goto(url)
+  // console.log('title:', title);
+  await browser.close()
+  // return title
 }
+
+// function testRedirect(url) {
+//   let xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function(e) {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       if (url != xhr.responseURL) {
+//         console.log('redirect detected to: ', xhr.responseURL)
+//       } else {
+//         console.log(
+//           'no redirect'
+//         )
+//       }
+//     }
+//   }
+//   xhr.open('GET', url, true);
+//   xhr.send(null)
+// }
 
 app.listen(4000, () => console.log(`listening to port 4000`))
