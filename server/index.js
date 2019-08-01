3a4d6b5f8307514a865d4c13cf62278b556075be
Student <jsitbon@qcc.cuny.edu>
2019-08-01 15:42:00 -0400

    trying to get the title back

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const addUrl = require('../models').addUrl
 const deleteUrl = require('../models').deleteUrl
 const updateCheck = require('../models').updateCheck
 const puppeteer = require('puppeteer')
+const axios = require('axios')
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -38,7 +39,8 @@ app.post('/title', async (req, res) => {
   let url = req.body.url;
   console.log('url', url)
   let title = await getTitlePup(url)
-  res.send(title)
+  // res.send(title)
+  axios.get(url).then(d => res.send(d.data)).catch(e => res.send(e.message))
 })
 
 
@@ -51,51 +53,19 @@ async function getTitlePup(url) {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   console.log('url in puppeteer', url)
-  let mainUrlStatus;
-  await page.setRequestInterception(true);
-  page.on("request", request => {
-    const url = request.url();
-    console.log("request url:", url);
-    request.continue();
-  });
-  page.on("requestfailed", request => {
-    const url = request.url();
-    console.log("request failed url:", url);
-  });
-  page.on("response", response => {
-    const request = response.request();
-    const url = request.url();
-    const status = response.status();
-    console.log("response url:", url, "status:", status);
-		if (url === mainUrl) {
-			mainUrlStatus = status;
-		}
-  });
-  // const title = await page.evaluate(() => {
-  //   return document.getElementsByTagName('title')[0].text
-  // })
+  await page.goto(url, {waitUntil: 'networkidle0'})
+  await page.waitFor(5000)
+  const title = await page.evaluate(() => {
+      return document
+      // console.log('evaluate', document.getElementsByTagName('body'))
+      // console.log(titles)
+      // return 'this is the title'
+  })
   
-  await page.goto(url)
   // console.log('title:', title);
   await browser.close()
-  // return title
+  console.log('title: ', title)
+  return title
 }
-
-// function testRedirect(url) {
-//   let xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = function(e) {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       if (url != xhr.responseURL) {
-//         console.log('redirect detected to: ', xhr.responseURL)
-//       } else {
-//         console.log(
-//           'no redirect'
-//         )
-//       }
-//     }
-//   }
-//   xhr.open('GET', url, true);
-//   xhr.send(null)
-// }
 
 app.listen(4000, () => console.log(`listening to port 4000`))
