45d7e851c5518276dfbdc4ca9ff43673ec4597a2
pisc9211 <pisc9211@gmail.com>
2019-08-08 15:15:57 -0400

    cccc

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 // if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/../build')))
+  
   
 // }
 
@@ -52,6 +52,7 @@ app.delete('/api', (req, res) => {
   res.send('deleted!')
 })
 
+app.use(express.static(path.join(__dirname, '/../build')))
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/../build/index.html'))
 // })
